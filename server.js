ca8a6a9ba139cbc6a6c269db35f4122d8b567265
andrew-gardner22 <165234990+andrew-gardner22@users.noreply.github.com>
2025-05-08 18:19:13 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+wss.on('connection', (ws) => {
+  ws.on('message', (msg) => {
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(msg);
+      }
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server running...');
+});
